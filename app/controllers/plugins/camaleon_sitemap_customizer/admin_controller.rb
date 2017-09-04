024cce19819224dc279c01e598e4c0a6b0402970
Brian Kephart <briantkephart@gmail.com>
2017-09-03 23:26:40 -0500

    add content type lists, category lists, tags, redundant homepage url to exclusion options

--- app/controllers/plugins/camaleon_sitemap_customizer/admin_controller.rb
+++ app/controllers/plugins/camaleon_sitemap_customizer/admin_controller.rb
@@ -9,11 +9,13 @@ class Plugins::CamaleonSitemapCustomizer::AdminController < CamaleonCms::Apps::P
 
   # save values from settings form
   def save_settings
-    if params[:options].present?
-      @plugin.set_options(params[:options]) # save option values
-    else
-      @plugin.set_options({'skip_post_types' => []}) # removed saved values if form is blank
-    end
+    params[:options]                             ||= {}
+    params[:options]['skip_post_types']          ||= []
+    params[:options]['skip_post_list_types']     ||= []
+    params[:options]['skip_category_list_types'] ||= []
+    params[:options]['skip_tags']                ||= false
+    params[:options]['skip_home']                ||= false
+    @plugin.set_options(params[:options]) # save option values
     @plugin.set_metas(params[:metas]) if params[:metas].present? # save meta values
     @plugin.set_field_values(params[:field_options]) if params[:field_options].present? # save custom field values
     redirect_to url_for(action: :settings), notice: 'Settings Saved Successfully'
--- app/helpers/plugins/camaleon_sitemap_customizer/main_helper.rb
+++ app/helpers/plugins/camaleon_sitemap_customizer/main_helper.rb
@@ -18,16 +18,23 @@ module Plugins::CamaleonSitemapCustomizer::MainHelper
 
   def customize_sitemap(r)
 
+    # completely excluded content types
     r[:skip_posttype_ids] += current_plugin.get_option('skip_post_types').map(&:to_i)
     r[:skip_posttype_ids].each do |ptype|
-      # leave out post pages within post type
+      # leave out post pages within content type
       current_site.the_posts(ptype).each do |post|
         r[:skip_post_ids] += [post.id]
       end
-      # leave out category pages within post type
+      # leave out category pages within content type
       current_site.the_categories(ptype).each do |cat|
         r[:skip_cat_ids] += [cat.id]
       end
     end
+
+    # excluded list pages
+    r[:skip_posttype_ids] += current_plugin.get_option('skip_post_list_types')    .map(&:to_i)
+    r[:skip_cat_ids]      += current_plugin.get_option('skip_category_list_types').map(&:to_i)
+    r[:skip_tag_ids]      += current_site.the_tags.map(&:id)   if current_plugin.get_option('skip_tags')
+    r[:skip_post_ids]     += [@_site_options[:home_page].to_i] if current_plugin.get_option('skip_home')
   end
 end
