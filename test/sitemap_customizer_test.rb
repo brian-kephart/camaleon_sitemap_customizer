cbf7c700e7f18a27ae1469a3e357ed51164b9d5c
Brian Kephart <briantkephart@gmail.com>
2018-05-20 15:54:47 -0500

    Add RuboCop to Travis CI build, and only run tests on current and future versions of Ruby

--- test/sitemap_customizer_test.rb
+++ test/sitemap_customizer_test.rb
@@ -4,8 +4,4 @@ class CamaleonSitemapCustomizer::Test < ActiveSupport::TestCase
   test 'truth' do
     assert_kind_of Module, CamaleonSitemapCustomizer
   end
-
-  test 'RuboCop finds no syntax errors' do
-    assert_match /no offenses detected/, `rubocop --only Syntax`, 'RuboCop detected syntax errors. Run `rubocop --only Syntax` to find and fix these errors'
-  end
 end
